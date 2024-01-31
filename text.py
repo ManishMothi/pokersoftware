6a0a61c0a19a2fcc15d3418902f0d0413e5b5025
Manish Mothi <Mmanish0881@gmail.com>
2024-01-31 14:56:12 -0500

    Initial commit with poker software and hand prediction

---
+++ text.py
@@ -0,0 +1,16 @@
+# This will be converted to a README later on
+
+'''
+https://github.com/HenryRLee/PokerHandEvaluator
+
+http://suffe.cool/poker/evaluator.html
+The algorithm for calculating poker hands was very interesting with the use of prime factor multiplication for efficiency
+
+
+
+
+
+
+
+
+'''
